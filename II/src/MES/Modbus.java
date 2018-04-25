04c2a5ec251845b4963618d004502fc8c4a24834
ruimendes45 <ruimendes45@gmail.com>
2018-04-25 13:56:03 +0100

    Update Modbus.java
    Fiz as seguintes alterações: 
    -Rotina try(), catch() para detetar erros na execução do código;
    -Função Ler Registos;

--- II/src/MES/Modbus.java
+++ II/src/MES/Modbus.java
@@ -1,4 +1,4 @@
-package MES;
+package mes;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
@@ -8,26 +8,43 @@ import de.re.easymodbus.modbusclient.*;
 
 public class Modbus {
 	
+	ModbusClient modbusClient = new ModbusClient();
 
-
-
-	public static void main(String[] args) throws UnknownHostException, IOException, ModbusException
-
+	public static int main(String[] args) throws UnknownHostException, IOException, ModbusException
+	{
+	ModbusClient modbusClient = new ModbusClient();
+	
+	try
 	{
 	
-
-		ModbusClient modbusClient = new ModbusClient();
 		modbusClient.Connect("172.22.129.102",502);
 	
+		return 1;
+				
+	}
+	catch (IOException e)
+	{
+		System.out.println("Erro de conexão");
+	
+	}
+		return 0;
+	}
 
-		int[] inputRegisters = modbusClient.ReadInputRegisters(0,  10);
+	/*
+	 * Função com o objetivo de ler registos
+	 */
+	public static int[] Ler_Saida_Bool(int registo, int n) throws Exception {
+	
+		ModbusClient modbusClient = new ModbusClient();
+
+		int[] saida = modbusClient.ReadInputRegisters(registo,  n);
 		
-		for (int i = 0; i < inputRegisters.length; i++)
+		for (int i = 0; i < saida.length; i++)
 			
-		System.out.println("Input Register #"+i+": "+inputRegisters[i]);
+		System.out.println("Input Register #"+i+": "+saida[i]);
 		
 		modbusClient.WriteSingleRegister(3,100);
-
-	
+		
+		return saida;
 }
 }
